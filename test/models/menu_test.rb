910b89d8592991bf354ffacd91a18e54d026249c
Carter Olson <carterolson15@gmail.com>
2022-02-13 13:29:35 -0600

    create migration to change float to decimal

--- test/models/menu_test.rb
+++ test/models/menu_test.rb
@@ -4,4 +4,17 @@ class MenuTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    
+  end
+
+  test "menu has many menuitems" do
+    menu = Menu.new 
+    item1 = MenuItem.new :name => "Chicken Fingers",
+                        :price => 500.25
+
+    item2 = MenuItem.new :name => "Burger",
+                        :price => 5.25
+    menu.menu_items << sc
+  end
 end
